b14622d040e1c5c85897efa5a12cd2f5e77bfdeb
MateusVieira <mateusvieira.tf@gmail.com>
2020-10-04 19:08:23 -0300

    Update NotasSemestrais.java

--- Projeto1/NotasSemestrais.java
+++ Projeto1/NotasSemestrais.java
@@ -19,51 +19,51 @@ public class SisGestao {
 //notas
 //1B
     System.out.print("Digite a Nota da Prova 1: ");
-    float nProva1 = Float.valueOf(teclado.nextLine());
+        float nProva1 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota de Projeto 1: ");
-    float nProj1 = Float.valueOf(teclado.nextLine());
+        float nProj1 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota da Lista de Exercícios 1: ");
-    float nlisExe1 = Float.valueOf(teclado.nextLine());
+        float nlisExe1 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota de Contribuição em Projetos/MOOCs 1: ");
-    float ncontPS1 = Float.valueOf(teclado.nextLine());
+        float ncontPS1 = Float.valueOf(teclado.nextLine());
     System.out.println("=======================================");    
  //2B
-   System.out.print("Digite a Nota da Prova 2: ");
-    float nProva2 = Float.valueOf(teclado.nextLine());
+    System.out.print("Digite a Nota da Prova 2: ");
+        float nProva2 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota de Projeto 2: ");
-    float nProj2 = Float.valueOf(teclado.nextLine());
+        float nProj2 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota da Lista de Exercícios 2: ");
-    float nlisExe2 = Float.valueOf(teclado.nextLine());
+        float nlisExe2 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota de Contribuição em Projetos/MOOCs 2: ");
-    float ncontPS2 = Float.valueOf(teclado.nextLine());
+        float ncontPS2 = Float.valueOf(teclado.nextLine());
     System.out.println("=======================================");
  //3B
-   System.out.print("Digite a Nota da Prova 3: ");
-    float nProva3 = Float.valueOf(teclado.nextLine());
+    System.out.print("Digite a Nota da Prova 3: ");
+        float nProva3 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota de Projeto 3: ");
-    float nProj3 = Float.valueOf(teclado.nextLine());
+        float nProj3 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota da Lista de Exercícios 3: ");
-    float nlisExe3 = Float.valueOf(teclado.nextLine());
+        float nlisExe3 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota de Contribuição em Projetos/MOOCs 3: ");
-    float ncontPS3 = Float.valueOf(teclado.nextLine());
+        float ncontPS3 = Float.valueOf(teclado.nextLine());
     System.out.println("=======================================");
  //4B
-   System.out.print("Digite a Nota da Prova 4: ");
-    float nProva4 = Float.valueOf(teclado.nextLine());
+    System.out.print("Digite a Nota da Prova 4: ");
+        float nProva4 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota de Projeto 4: ");
-    float nProj4 = Float.valueOf(teclado.nextLine());
+        float nProj4 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota da Lista de Exercícios 4: ");
-    float nlisExe4 = Float.valueOf(teclado.nextLine());
+        float nlisExe4 = Float.valueOf(teclado.nextLine());
     System.out.print("Digite a Nota de Contribuição em Projetos/MOOCs 4: ");
-    float ncontPS4 = Float.valueOf(teclado.nextLine());
+        float ncontPS4 = Float.valueOf(teclado.nextLine());
     System.out.println("=======================================");
         
 //Médias
-float mB1 = ((nProva1*3)+(nProj1*3)+(nlisExe1*2)+(ncontPS1*3))/11;
-float mB2 = ((nProva2*3)+(nProj2*3)+(nlisExe2*2)+(ncontPS2*3))/11;
-float mB3 = ((nProva3*3)+(nProj3*3)+(nlisExe3*2)+(ncontPS3*3))/11;
-float mB4 = ((nProva4*3)+(nProj4*3)+(nlisExe4*2)+(ncontPS4*3))/11;
-float mSem = Math.round((mB1+mB2+mB3+mB4)/4);
+    float mB1 = ((nProva1*3)+(nProj1*3)+(nlisExe1*2)+(ncontPS1*3))/11;
+    float mB2 = ((nProva2*3)+(nProj2*3)+(nlisExe2*2)+(ncontPS2*3))/11;
+    float mB3 = ((nProva3*3)+(nProj3*3)+(nlisExe3*2)+(ncontPS3*3))/11;
+    float mB4 = ((nProva4*3)+(nProj4*3)+(nlisExe4*2)+(ncontPS4*3))/11;
+    float mSem = Math.round((mB1+mB2+mB3+mB4)/4);
     System.out.println("A média semestral de " + nome + " é " + mSem);
     
 if (mSem >= 8) {
