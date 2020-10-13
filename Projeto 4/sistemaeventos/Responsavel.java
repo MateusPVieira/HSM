7276ee34ba93ad3b5293e42a59c492915a730007
MateusVieira <mateusvieira.tf@gmail.com>
2020-10-13 02:01:55 -0300

    Update Responsavel.java

---
+++ Projeto 4/sistemaeventos/Responsavel.java
@@ -0,0 +1,54 @@
+package sistemaeventos;
+
+/**
+ *
+ * @author math_
+ */
+public class Responsavel {
+    private String nomeResp;
+    private String contatoResp;
+    private float valorHoraResp;
+    private String info;
+    
+//Métodos
+
+    public Responsavel() {
+    }
+
+    public String getNomeResp() {
+        return nomeResp;
+    }
+
+    public void setNomeResp(String nomeResp) {
+        this.nomeResp = nomeResp;
+    }
+
+    public String getContatoResp() {
+        return contatoResp;
+    }
+
+    public void setContatoResp(String contatoResp) {
+        this.contatoResp = contatoResp;
+    }
+
+    public float getValorHoraResp() {
+        return valorHoraResp;
+    }
+
+    public void setValorHoraResp(float valorHoraResp) {
+        this.valorHoraResp = valorHoraResp;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    
+    
+    
+    
+    
+}
